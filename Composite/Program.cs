d8a62ab8e8b5ec7f661d09363ae3fd611330eef9
Вершковд <@403_3>
2018-07-14 13:19:15 +0600

    c3

---
+++ Composite/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    class Program
+    {
+        public interface Developer
+        {
+            void WriteCode();
+        }
+
+        public class CPPDeveloper : Developer
+        {
+            public void WriteCode()
+            {
+                Console.WriteLine("c++ developer");
+            }
+        }
+
+        public class JavaDeveloper : Developer
+        {
+            public void WriteCode()
+            {
+                Console.WriteLine("java developer");
+            }
+        }
+
+        public class TeamComposite : Developer
+        {
+            private string Name;
+            public TeamComposite(string name)
+            {
+                Name = name;
+            }
+
+            private List<Developer> Developers = new List<Developer>();
+            public void AddDeveloper(Developer dev)
+            {
+                Developers.Add(dev);
+            }
+
+            public void RemoveDeveloper(Developer dev)
+            {
+                Developers.Remove(dev);
+            }
+
+            public void WriteCode()
+            {
+                Console.WriteLine("Team: " + Name + " writes  project") ;
+                foreach (var item in Developers)
+                {
+                    item.WriteCode();
+                }
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
